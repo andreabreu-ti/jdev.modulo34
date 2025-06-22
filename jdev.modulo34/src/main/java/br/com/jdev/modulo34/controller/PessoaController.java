b88c39e3579f9447c03c2c73052c1494a57331c1
andre abreu <aanddre@gmail.com>
2025-06-22 17:35:44 -0300

    Implementando CRUD completo

--- jdev.modulo34/src/main/java/br/com/jdev/modulo34/controller/PessoaController.java
+++ jdev.modulo34/src/main/java/br/com/jdev/modulo34/controller/PessoaController.java
@@ -1,28 +1,119 @@
 package br.com.jdev.modulo34.controller;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 import br.com.jdev.modulo34.entity.Pessoa;
 import br.com.jdev.modulo34.repository.PessoaRepository;
 
 @Controller
 public class PessoaController {
-	
+
 	@Autowired
 	private PessoaRepository pessoaRepository;
 
 	@GetMapping("/cadastropessoa")
-	public String inicio() {
-		return "cadastro/cadastropessoa";
+	public ModelAndView inicio() {
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		modelAndView.addObject("pessoas", pessoasIt);
+		return modelAndView;
+	}
+
+	/**
+	 * Metodo para salvar e retornar uma lista
+	 * 
+	 * @param pessoa
+	 * @return
+	 */
+	@PostMapping("/salvarpessoa")
+	public ModelAndView salvar(Pessoa pessoa) {
+
+		pessoaRepository.save(pessoa);
+
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+
+		return andView;
+
+	}
+
+	/**
+	 * Método para listar todos os registros
+	 * 
+	 * @return
+	 */
+	@GetMapping(value = "/listapessoas")
+	public ModelAndView pessoas() {
+		ModelAndView andView = new ModelAndView("cadastro/cadastropessoa");
+		Iterable<Pessoa> pessoasIt = pessoaRepository.findAll();
+		andView.addObject("pessoas", pessoasIt);
+		andView.addObject("pessoaobj", new Pessoa());
+		return andView;
+	}
+
+	/**
+	 * Metodo para eidtar um id
+	 * 
+	 * @param idpessoa
+	 * @return
+	 */
+	@GetMapping("/editarpessoa/{idpessoa}")
+	public ModelAndView editar(@PathVariable("idpessoa") Long idpessoa) {
+
+		Optional<Pessoa> pessoa = pessoaRepository.findById(idpessoa);
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoaobj", pessoa.get());
+		return modelAndView;
+
+	}
+
+	/**
+	 * Metodo para excluir id
+	 * 
+	 * @param idpessoa
+	 * @return
+	 */
+	@GetMapping("/removerpessoa/{idpessoa}")
+	// @DeleteMapping("/removerpessoa/{idpessoa}")
+	public ModelAndView excluir(@PathVariable("idpessoa") Long idpessoa) {
+
+		pessoaRepository.deleteById(idpessoa);
+
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas", pessoaRepository.findAll());
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
+
 	}
 	
-	@PostMapping("/salvarpessoa")
-	public String salvar(Pessoa pessoa) {
-		pessoaRepository.save(pessoa);
-		return "cadastro/cadastropessoa";
+	/**
+	 * Metodo para pesquisar por parte do nome
+	 * 
+	 * @param nomepesquisa
+	 * @return
+	 */
+	@PostMapping("/pesquisarpessoa")
+	public ModelAndView pesquisa(@RequestParam("nomepesquisa") String nomepesquisa) {
+		
+		ModelAndView modelAndView = new ModelAndView("cadastro/cadastropessoa");
+		modelAndView.addObject("pessoas", pessoaRepository.findPessoaByName(nomepesquisa));
+		modelAndView.addObject("pessoaobj", new Pessoa());
+		return modelAndView;
+				
 	}
 
 }
--- jdev.modulo34/src/main/java/br/com/jdev/modulo34/repository/PessoaRepository.java
+++ jdev.modulo34/src/main/java/br/com/jdev/modulo34/repository/PessoaRepository.java
@@ -1,6 +1,9 @@
 package br.com.jdev.modulo34.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.jdev.modulo34.entity.Pessoa;
@@ -8,4 +11,12 @@ import br.com.jdev.modulo34.entity.Pessoa;
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
 
+	/**
+	 * Consulta customizada para pesquisar por parte do nome
+	 * 
+	 * @param nome
+	 * @return
+	 */
+	@Query("select p from Pessoa p where p.nome like %?1%") 
+	List<Pessoa> findPessoaByName(String nome);
 }
